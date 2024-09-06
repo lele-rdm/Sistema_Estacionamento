6d5f39b1ff27eab59ae1e689aa728107d3ca1f98
Leandro Martins <leandromartinspinto2004@gmail.com>
2024-09-06 13:31:11 -0300

    Projeto Finalizado.

---
+++ Entities/Estacionamento.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes.Entities
+{
+    class Estacionamento
+    {
+        public decimal PrecoInicial { get; private set; }
+        public decimal PrecoPorHora { get; private set; }
+
+        private List<string> Veiculos = new();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            PrecoInicial = precoInicial;
+            PrecoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.Write("\n Digite a placa do veículo: ");
+            string placa = Console.ReadLine();
+            Veiculos.Add(placa);
+            Console.WriteLine("\n Adicionando veículo...");
+            Thread.Sleep(2000);
+            Console.WriteLine(" Veículo adicionado com sucesso! ");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.Write("\n Digite a placa do veículo: ");
+            string placa = Console.ReadLine();
+            Console.Write(" Digite a quantidade de horas utilizadas: ");
+            int horasEstacionadas = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n Removendo veículo...");
+            Thread.Sleep(2000);
+            Console.WriteLine($"\n O veículo {placa} foi removido com sucesso! ");
+            Console.WriteLine($" O preço total foi de: R${CalcularPrecoTotal(horasEstacionadas)}");
+            Veiculos.Remove(placa);
+            Console.Write("\nPressione enter para voltar: ");
+            string resp = Console.ReadLine();
+            if (resp == "")
+            {
+                Console.Clear();
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            Console.WriteLine("\n --- Veículos Adicionados ---\n");
+            foreach (string v in Veiculos)
+            {
+                Console.WriteLine($" {v}");
+            }
+            Console.Write("\nPressione enter para voltar: ");
+            string resp1 = Console.ReadLine();
+            if (resp1 == "")
+            {
+                Console.Clear();
+            }
+        }
+
+        public void Sair()
+        {
+            Console.WriteLine("\n Saindo...");
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+        }
+
+        public decimal CalcularPrecoTotal(decimal horasEstacionadas)
+        {
+            decimal precoTotal = PrecoInicial + PrecoPorHora * horasEstacionadas;
+            return precoTotal;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using Testes.Entities;
+
+Console.WriteLine("\n --- SEJA BEM VINDO AO SISTEMA DE ESTACIONAMENTO --- \n");
+
+Console.Write(" Digite o preço inicial: R$");
+decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+Console.Write(" Digite o preço por hora: R$");
+decimal precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+Console.Clear();
+
+while (true)
+{
+    Console.WriteLine("\n 1 - Cadastrar Veículo");
+    Console.WriteLine(" 2 - Remover Veículo");
+    Console.WriteLine(" 3 - Listar Veículos");
+    Console.WriteLine(" 4 - Sair");
+
+    Console.Write("\n Digite a sua opção: ");
+    int opcao = int.Parse(Console.ReadLine());
+    Console.Clear();
+
+    switch (opcao)
+    {
+        case 1:
+            es.AdicionarVeiculo();
+            break;
+        case 2:
+            es.RemoverVeiculo();
+
+            break;
+        case 3:
+            es.ListarVeiculos();
+            break;
+        case 4:
+            es.Sair();
+            break;
+        default:
+            Console.WriteLine("Opção Inválida.");
+            break;
+    }
+}
